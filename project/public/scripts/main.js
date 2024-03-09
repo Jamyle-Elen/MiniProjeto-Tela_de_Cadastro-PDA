e43f760df6c4072dd82a9b101e58085cc357cb02
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-03-09 08:09:43 -0300

    itemName error (arrumar)

--- project/public/scripts/main.js
+++ project/public/scripts/main.js
@@ -14,28 +14,47 @@ const itemImageMap = {
 const displayItems = new Set();
 
 function mostrarImagem() {
-    const itemNameInput = document.getElementById('item').value.trim();
-    const itemName = itemNameInput.toLowerCase();
-    const imagePath = itemImageMap[itemName];
+    const itemNameInput = document.getElementById('item').value.trim().toLowerCase();
+    const item = itemImageMap[itemNameInput];
     const imagesContainer = document.querySelectorAll('.itemImage');
 
-    console.log('Nome do Item:', itemName);
+    console.log('Nome do Item:', itemNameInput);
 
-    if (imagePath && !displayItems.has(itemName) && displayItems.size < 6) {
-        const availableSlot = Array.from(imagesContainer).findIndex(img => img.src === '');
+    if (item && !displayItems.has(itemNameInput) && displayItems.size < 6) {
+        const availableSlot = Array.from(imagesContainer).findIndex(img => !img.src);
         if (availableSlot !== -1) {
             const imgId = `itemImage${availableSlot + 1}`;
             const itemImage = document.getElementById(imgId);
 
-            itemImage.src = imagePath.imagePath;
+            itemImage.src = item.imagePath;
             itemImage.alt = `Imagem do item ${availableSlot + 1}`;
             itemImage.style.display = 'block';
 
-            displayItems.add(itemName);
+            displayItems.add(itemNameInput);
+            
+            itemImage.addEventListener('mouseover', () => mostrarNomeItem(imgId, item.name));
+            itemImage.addEventListener('mouseout', () => esconderNomeItem(imgId));
         } else {
             console.error('Não há slots disponíveis para exibir a imagem.');
         }
     } else {
         console.error('Este item não foi encontrado ou já foi adicionado');
+    }
+}
+
+function mostrarNomeItem(imageId, itemName) {
+    const abbrElement = document.createElement('abbr');
+    abbrElement.textContent = itemName;
+    abbrElement.title = itemName;
+    document.getElementById(imageId).parentNode.appendChild(abbrElement);
+}
+
+function esconderNomeItem(imageId) {
+    const abbrElement = document.getElementById(imageId).parentNode.querySelector('abbr');
+    if (abbrElement) {
+        setTimeout(() => {
+            abbrElement.remove();
+        },1);
+        
     }
 }
