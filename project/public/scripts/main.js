51e162ee9e066d3619eac9cc9e792ad6b09c630b
Jamyle <110051309+Jamyle-Elen@users.noreply.github.com>
2024-03-10 22:21:34 -0300

    teste

--- project/public/scripts/main.js
+++ project/public/scripts/main.js
@@ -1,52 +1,54 @@
-
 class Item {
-    constructor(name, imagePath) {
-        this.name = name;
-        this.imagePath = imagePath;
-    }
+  constructor(name, imagePath) {
+    this.name = name;
+    this.imagePath = imagePath;
+  }
 }
 
 const itemImageMap = {
-    "proto": new Item("proto", "images/itens-lol/protocinturao-item.png"),
-    "eco": new Item("eco", "images/itens-lol/eco-item.webp"),
-    "eco2": new Item("eco", "images/itens-lol/eco-item.webp"),
-    "eco3": new Item("eco", "images/itens-lol/eco-item.webp"),
-    "eco4": new Item("eco", "images/itens-lol/eco-item.webp"),
-    "eco5": new Item("eco", "images/itens-lol/eco-item.webp"),
-    "eco6": new Item("eco", "images/itens-lol/eco-item.webp")
+  proto: new Item("proto", "images/itens-lol/protocinturao-item.png"),
+  eco: new Item("eco", "images/itens-lol/eco-item.webp"),
+  eco2: new Item("eco", "images/itens-lol/eco-item.webp"),
+  eco3: new Item("eco", "images/itens-lol/eco-item.webp"),
+  eco4: new Item("eco", "images/itens-lol/eco-item.webp"),
+  eco5: new Item("eco", "images/itens-lol/eco-item.webp"),
+  eco6: new Item("eco", "images/itens-lol/eco-item.webp"),
 };
 
 const displayItems = new Set();
 
 function mostrarImagem() {
-    const itemNameInput = document.getElementById('item').value.trim().toLowerCase();
-    const item = itemImageMap[itemNameInput];
-    const imagesContainer = document.querySelector('.imagens');
+  const itemNameInput = document
+    .getElementById("item")
+    .value.trim()
+    .toLowerCase();
+  const item = itemImageMap[itemNameInput];
+  const imagesContainer = document.querySelector(".imagens");
 
-    console.log('Nome do Item:', itemNameInput);
+  console.log("Nome do Item:", itemNameInput);
 
-    if (item && !displayItems.has(itemNameInput) && displayItems.size < 6) {
-        const availableSlot = displayItems.size + 1;
-        const imgId = `itemImage${availableSlot}`;
-        let itemImage = document.getElementById(imgId);
+  if (item && !displayItems.has(itemNameInput) && displayItems.size < 6) {
+    const availableSlot = displayItems.size + 1;
+    const imgId = `itemImage${availableSlot}`;
+    let itemImage = document.getElementById(imgId);
 
-        if (!itemImage) {
-            itemImage = document.createElement('img');
-            itemImage.id = imgId;
-            itemImage.className = 'itemImage';
-            itemImage.alt = `Imagem do item ${availableSlot}`;
-            imagesContainer.appendChild(itemImage);
-        }
-
-        itemImage.src = item.imagePath;
-        itemImage.style.display = 'block';
-
-        displayItems.add(itemNameInput);
-    } else {
-        console.error('Este item não foi encontrado ou já foi adicionado');
+    if (!itemImage) {
+      itemImage = document.createElement("img");
+      itemImage.id = imgId;
+      itemImage.className = "itemImage";
+      itemImage.alt = `Imagem do item ${availableSlot}`;
+      imagesContainer.appendChild(itemImage);
     }
 
-    document.querySelector('.imagens').classList.add('selling-mode');
+    itemImage.src = item.imagePath;
+    itemImage.style.display = "block";
+
+    displayItems.add(itemNameInput);
+  } else {
+    console.error("Este item não foi encontrado ou já foi adicionado");
+  }
+
+  document.querySelector(".imagens").classList.add("selling-mode");
 }
 
 // function editarItem(imageId) {
@@ -66,105 +68,136 @@ function mostrarImagem() {
 
 // Vender itens
 function venderItens() {
-    const imagensContainer = document.querySelector('.imagens');
-    const buildNameInput = document.getElementById('buildName').value.trim();
+  const imagensContainer = document.querySelector(".imagens");
 
-    imagensContainer.querySelectorAll('.itemImage').forEach((itemImage, index) => {
-        itemImage.src = '';
-        itemImage.style.display = 'none';
 
-        const deleteIcon = imagensContainer.querySelector(`#deleteIcon${index + 1}`);
-        if (deleteIcon) {
-            deleteIcon.remove();
-        }
+  imagensContainer
+    .querySelectorAll(".itemImage")
+    .forEach((itemImage, index) => {
+      itemImage.src = "";
+      itemImage.style.display = "none";
+
+      const deleteIcon = imagensContainer.querySelector(
+        `#deleteIcon${index + 1}`
+      );
+      if (deleteIcon) {
+        deleteIcon.remove();
+      }
     });
 
-    imagensContainer.innerHTML = '';
+  imagensContainer.innerHTML = "";
 
-    const imageContainers = imagensContainer.querySelectorAll('.itemImage');
-    imageContainers.forEach((imageContainer, index) => {
-        const deleteIcon = document.createElement('i');
-        deleteIcon.id = `deleteIcon${index + 1}`;
-        deleteIcon.className = 'bx bx-x delete-icon';
-        deleteIcon.addEventListener('click', () => deletarItem(displayItems[index], `itemImage${index + 1}`));
-        imageContainer.parentNode.appendChild(deleteIcon);
+  const imageContainers = imagensContainer.querySelectorAll(".itemImage");
+  imageContainers.forEach((imageContainer, index) => {
+    const deleteIcon = document.createElement("i");
+    deleteIcon.id = `deleteIcon${index + 1}`;
+    deleteIcon.className = "bx bx-x delete-icon";
+    deleteIcon.addEventListener("click", () =>
+      deletarItem(displayItems[index], `itemImage${index + 1}`)
+    );
+    imageContainer.parentNode.appendChild(deleteIcon);
+  });
+
+  displayItems.clear();
+
+  const build = {
+    name: buildNameInput,
+    items: Array.from(displayItems)
+};
+
+  if (displayItems.size === 6 && buildNameInput !== "") {
+    console.log("Build Salva:", build);
+    miniaturasContainer.innerHTML = "";
+    displayItems.forEach((itemName) => {
+      const miniaturaImg = document.createElement("img");
+      const item = itemImageMap[itemName];
+      miniaturaImg.src = item.imagePath;
+      miniaturaImg.alt = `Miniatura do ${itemName} na build ${buildNameInput}`;
+      miniaturasContainer.appendChild(miniaturaImg);
     });
 
     displayItems.clear();
-
-    if (displayItems.size === 6 && buildNameInput !== '') {
-        const build = {
-            name: buildNameInput,
-            items: Array.from(displayItems)
-        };
-
-        console.log('Build Salva:', build);
-        miniaturasContainer.innerHTML = '';
-        displayItems.forEach(itemName => {
-            const miniaturaImg = document.createElement('img');
-            const item = itemImageMap[itemName];
-            miniaturaImg.src = item.imagePath;
-            miniaturaImg.alt = `Miniatura do ${itemName} na build ${buildNameInput}`;
-            miniaturasContainer.appendChild(miniaturaImg);
-        });
-
-        displayItems.clear();
-        document.querySelector('.imagens').classList.remove('selling-mode');
-    } else {
-        console.error('Selecione seis itens e insira um nome para salvar a build.');
-    }
+    document.querySelector(".imagens").classList.remove("selling-mode");
+  } else {
+    console.error("Selecione seis itens e insira um nome para salvar a build.");
+  }
 }
 
-// Comprar itens
+// Comprar itens - FUNCIONANDO
 function comprarItens() {
-    const imagensContainer = document.querySelector('.imagens');
-    const buildNameInput = document.getElementById('buildName').value.trim();
+  const imagensContainer = document.querySelector(".imagens");
+  const miniaturasContainer = document.getElementById("miniaturasContainer");
+  const buildNameInput = prompt("Nome da build.:");
 
-    imagensContainer.querySelectorAll('.itemImage').forEach((itemImage, index) => {
-        itemImage.src = '';
-        itemImage.style.display = 'none';
 
-        const deleteIcon = imagensContainer.querySelector(`#deleteIcon${index + 1}`);
-        if (deleteIcon) {
-            deleteIcon.remove();
-        }
-    });
+  // verifica se foram seis itens
+  if (displayItems.size === 6) {
+    if (buildNameInput !== null && buildNameInput.trim() !== "") {
+      const buildDiv = document.createElement("div");
+      buildDiv.className = "build-salva";
+      miniaturasContainer.appendChild(buildDiv);
 
-    const miniaturasContainer = document.getElementById('miniaturasContainer');
-    const buildsSalvasContainer = document.getElementById('buildsSalvas');
+      displayItems.forEach((itemName) => {
+        const miniaturaImg = document.createElement("img");
+        miniaturaImg.className = "miniatura-imagem";
+        const item = itemImageMap[itemName];
+        miniaturaImg.src = item.imagePath;
+        miniaturaImg.alt = `Miniatura do ${itemName} na build ${buildNameInput}`;
+        miniaturasContainer.appendChild(miniaturaImg);
+      });
 
-    if (buildNameInput !== '') {
-        // vai criar uma nova div pra cada nova build
-        const buildDiv = document.createElement('div');
-        buildDiv.className = 'build-salva';
+      imagensContainer.innerHTML = "";
 
-        displayItems.forEach(itemName => {
-            const miniaturaImg = document.createElement('img');
-            const item = itemImageMap[itemName];
-            miniaturaImg.src = item.imagePath;
-            miniaturaImg.alt = `Miniatura do ${itemName} na build ${buildNameInput}`;
-            miniaturasContainer.appendChild(miniaturaImg);
-        });
+      const buildNameElement = document.createElement("div");
+      buildNameElement.className = "build-name";
+      buildNameElement.textContent = `Build: ${buildNameInput}`;
+      buildDiv.appendChild(buildNameElement);
 
-        buildsSalvasContainer.appendChild(buildDiv);
+      // deletar build
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Excluir";
+      deleteButton.addEventListener("click", () => deletarBuild(buildDiv));
+      buildDiv.appendChild(deleteButton);
 
-        const build = {
-            name: buildNameInput,
-            items: Array.from(displayItems)
-        };
+      // editar nome da build
+      const editButton = document.createElement("button");
+      editButton.textContent = "Editar";
+      editButton.addEventListener("click", () => editarBuild(buildNameElement));
+      buildDiv.appendChild(editButton);
 
-        console.log('Build Salva:', build);
-
-        // onde as miniaturas vao ficar (criar divs)
-        const buildNameElement = document.createElement('div');
-        buildNameElement.textContent = `Build: ${buildNameInput}`;
-        buildsSalvasContainer.appendChild(buildNameElement);
-        
-        // limpar
-        displayItems.clear();
-        document.querySelector('.imagens').classList.remove('selling-mode');
+      document.querySelector(".imagens").classList.remove("selling-mode");
+      displayItems.clear();
     } else {
-        console.error('Insira um nome para salvar a build.');
+      console.error("Nome da build inválido.");
     }
+  } else {
+    console.error("Selecione seis itens antes de comprar a build.");
+  }
 }
 
+// function salvarEdicaoBuild() {
+//     const novoNome = document.getElementById('novoNomeBuild').value.trim();
+
+//     if (novoNome !== '') {
+//         // faz a att p original
+//         const buildNameElement = document.querySelector('.build-name');
+//         buildNameElement.textContent = `Build: ${novoNome}`;
+
+//         fecharModalEditar();
+//     }
+
+const build = {
+  name: buildNameInput,
+  items: Array.from(displayItems),
+};
+
+console.log("Build Salva:", build);
+
+// // onde as miniaturas vao ficar (criar divs)
+// const buildNameElement = document.createElement('div');
+// buildNameElement.textContent = `Build: ${buildNameInput}`;
+// buildsSalvasContainer.appendChild(buildNameElement);
+
+// limpar
+
+// imagens ainda tao ficando na mesma linha - arrumar isso depos
